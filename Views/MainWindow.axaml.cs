1dae98beb5cf6f4f01406f8bd3a55efa2be4cc7b
Mike K <rocker@gazeta.pl>
2024-12-04 17:00:13 +0100

    Add AvaloniaUI initial project files

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace TimeLapserdak.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
