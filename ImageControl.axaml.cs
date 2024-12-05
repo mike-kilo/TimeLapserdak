975543431fec26c6b9e68f0bd496074b46ae634b
Mike K <rocker@gazeta.pl>
2024-12-05 21:18:50 +0100

    Add ImageControl user control

---
+++ ImageControl.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TimeLapserdak;
+
+public partial class ImageControl : UserControl
+{
+    public ImageControl()
+    {
+        InitializeComponent();
+    }
+}
---
+++ ViewModels/ImageControlViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeLapserdak.ViewModels
+{
+    public partial class ImageControlViewModel : ViewModelBase, INotifyPropertyChanged
+    {
+		private Rect _imageCrop;
+
+		public Rect ImageCrop
+		{
+			get => _imageCrop; 
+			set 
+			{ 
+				_imageCrop =value; 
+				OnPropertyChanged(nameof(ImageCrop));
+			}
+		}
+	}
+}
