6d1d7d18265061ee1afdc0a25c216e00b3969536
Mike K <rocker@gazeta.pl>
2024-12-15 22:55:51 +0100

    Add methods for converting images to video

--- ImageProcessing.cs
+++ ImageProcessing.cs
@@ -1,6 +1,12 @@
 ﻿using Avalonia;
+using FFMpegCore;
+using FFMpegCore.Extensions.SkiaSharp;
+using FFMpegCore.Pipes;
 using SkiaSharp;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace TimeLapserdak
 {
@@ -20,6 +26,30 @@ namespace TimeLapserdak
                 
             using var data = outputBitmap.Encode(SKEncodedImageFormat.Jpeg, 95);
             File.WriteAllBytes(Path.Combine(outputFolder, file.Name), data.ToArray());
+        }
+
+        public static IEnumerable<BitmapVideoFrameWrapper> CreateFrames(List<FileInfo> images)
+        {
+            foreach (FileInfo file in images)
+            {
+                using var bitmap = SKBitmap.Decode(file.FullName);
+                using BitmapVideoFrameWrapper wrappedBitmap = new(bitmap);
+                yield return wrappedBitmap;
+            }
+        }
+
+        public static async Task<bool> GenerateVideo(IEnumerable<IVideoFrame> frames, string outputFolder)
+        {
+            RawVideoPipeSource source = new(frames) { FrameRate = 25 };
+            bool success = await FFMpegArguments
+                .FromPipeInput(source)
+                .OutputToFile(
+                    Path.Combine(outputFolder, "TimeLapserdak." + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".mp4"), 
+                    overwrite: true, 
+                    options => options.WithVideoCodec("mpeg4").UsingMultithreading(true).WithVideoBitrate(3500).WithSpeedPreset(FFMpegCore.Enums.Speed.UltraFast))
+                .ProcessAsynchronously();
+
+            return success;
         }
     }
 }
